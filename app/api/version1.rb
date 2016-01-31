5b2464855f95d1bb1684cb565b80c5d4f0ea2f07
Jiayi Zheng <thebluber@gmail.com>
2016-01-31 05:16:49 +0100

    Add post /save action for saving samples directly on server

--- app/api/version1.rb
+++ app/api/version1.rb
@@ -16,5 +16,23 @@ module API
 
       { scores: scores.map{ |score| score.last[:value] }, time: Time.now - start }
     end
+
+    desc 'Save samples'
+    params do
+      requires :sample, type: String
+    end
+    post 'save' do
+      error = nil
+      begin
+        JSON.parse(params[:sample])
+        file = File.open("test_samples/#{Time.now.hash.to_s(16).gsub("-", "")}", "w")
+        file.puts params[:sample]
+        file.close
+      rescue
+        error = "Can not parse json file!"
+      end
+
+      error ? { notice: error } : { notice: "Sample saved!" }
+    end
   end
 end
